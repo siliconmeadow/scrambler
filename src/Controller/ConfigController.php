e7ae1ac12fdf34f4989983bf5ea3ee548f7287b2
Novitsh <kevin@codephantom.net>
2015-11-06 16:38:05 +0100

    Issue by Novitsh: Changed word 'scramble' to 'scrambler' as it is reflecting to the module and no the name.

--- src/Controller/ConfigController.php
+++ src/Controller/ConfigController.php
@@ -19,8 +19,18 @@ class ConfigController {
    */
   public function getMenuItems() {
     return array(
+      'admin/config/development/scrambler' => array(
+        'title' => 'Configure Scrambler',
+        'description' => 'Configure general settings for scrambling.',
+        'page callback' => 'drupal_get_form',
+        'page arguments' => array('scrambler_general_settings_form'),
+        'type' => MENU_NORMAL_ITEM,
+        'access arguments' => array('administer scrambler'),
+        'file' => 'includes/scrambler.admin.inc',
+        'file path' => drupal_get_path('module', 'scrambler'),
+      ),
       'admin/config/development/scrambler/fields' =>  array(
-        'title' => 'Configure Fields Scramble',
+        'title' => 'Configure Fields Scrambler',
         'description' => 'Configure which fields need to be scrambled.',
         'page callback' => 'scrambler_administer_fields_page',
         'type' => MENU_NORMAL_ITEM,
@@ -29,7 +39,7 @@ class ConfigController {
         'file path' => drupal_get_path('module', 'scrambler'),
       ),
       'admin/config/development/scrambler/general' => array(
-        'title' => 'Configure General Scramble',
+        'title' => 'Configure General Scrambler',
         'description' => 'Configure general settings for scrambling.',
         'page callback' => 'drupal_get_form',
         'page arguments' => array('scrambler_general_settings_form'),
