af168714fb1ac88ad9acb1e63791d98ec79e1393
Novitsh <kevin@codephantom.net>
2015-11-07 09:13:12 +0100

    Issue #2610198 by Novitsh: Create a placeholder for example module.

---
+++ scrambler.api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ *
+ * API model for Scrambler.
+ */
+
+use Drupal\scrambler\Config;
+
+/**
+ * Declaration of hook_scrambler_api().
+ */
+function hook_scrambler_api() {
+  $api = array();
+
+  return $api;
+}
