a60e2051f0d0c427faf3dcd3c2e33edb2f5e24e0
Novitsh <kevin@codephantom.net>
2016-02-10 16:50:30 +0100

    Issue #2610162 by Novitsh: Updated standards for Security\Permission.

--- modules/scrambler_field/src/Security/Permission.php
+++ modules/scrambler_field/src/Security/Permission.php
@@ -1,23 +1,30 @@
 <?php
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @file
+ * Work with permissions.
  */
 
-/**
- * Description of Permission
- *
- * @author Greg Bakos <greg@londonfreelancers.co.uk>
- */
 namespace Drupal\scrambler_field\Security;
 
+/**
+ * Class Permission.
+ *
+ * @package Drupal\scrambler_field\Security
+ */
 class Permission {
+  /**
+   * Define permissions for Drupal.
+   *
+   * @return array
+   *   Array with permissions.
+   */
   public function getPermission() {
     return array(
       'administer scrambler fields' => array(
         'title' => t('Administer scrambler fields'),
         'description' => t('Configure the fields to be sanitized.'),
       ),
-    );    
+    );
   }
+
 }
