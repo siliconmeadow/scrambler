siliconmeadow/scrambler

commit 5a8d59f01832244eea0ccbcb9944685e36d307c2

Parreview: Fixed multiple parreview issues

src/API.php
@@ -85,7 +85,7 @@ class ImplementationObject {
    * @return array
    *   Returns all records where the methods have been applied.
    */
-  private function applyMethod($records) {
+  private function applyMethod(array $records) {
     $up_records = array();
     $function = $this->method;
 
@@ -113,12 +113,13 @@ class ImplementationObject {
    * @param int $value
    *   Field value of the key id to apply conditions.
    */
-  private function updateTable($table, $fields, $key, $value) {
+  private function updateTable($table, array $fields, $key, $value) {
     db_update($table)
       ->fields($fields)
       ->condition($key, $value)
       ->execute();
   }
+
 }
 
 /**
@@ -182,7 +183,7 @@ class API {
    * @param array $implementation
    *   Contains the implementation data structure array.
    */
-  private function scrambleImplementation($implementation) {
+  private function scrambleImplementation(array $implementation) {
     foreach ($implementation as $module => $groups) {
       $this->scrambleImplementationGroup($module, $groups);
     }
@@ -191,10 +192,12 @@ class API {
   /**
    * Scramble the implementation group.
    *
+   * @param string $module
+   *   Contains the module name.
    * @param array $groups
    *   Contains the implementation groups structure array.
    */
-  public function scrambleImplementationGroup($module, $groups) {
+  public function scrambleImplementationGroup($module, array $groups) {
     foreach ($groups as $group) {
       $object = $this->prepareScramblerObject($module, $group);
 
@@ -225,7 +228,7 @@ class API {
    * @return \Drupal\scrambler\ImplementationObject
    *   Returns an implementation object.
    */
-  public function prepareScramblerObject($module, $group) {
+  public function prepareScramblerObject($module, array $group) {
     $object = new ImplementationObject();
     // Name of the module.
     $object->module = $module;
@@ -256,7 +259,7 @@ class API {
    * @return string|bool
    *   Returns the method name or FALSE if the method name does not exist.
    */
-  private function getMethodName($module, $params) {
+  private function getMethodName($module, array $params) {
     // First check if the group method exists.
     $method = '_' . $module . '_method_' . $params['method'];
 

includes/scrambler.methods.inc
@@ -3,6 +3,7 @@
  * @file
  * Contains the default scrambler scramble methods.
  */
+
 // Define method constants.
 define('SCRAMBLER_METHOD_SHUFFLE_CHARACTERS', 'shuffle_characters');
 define('SCRAMBLER_METHOD_SHUFFLE_WORDS', 'shuffle_words');
@@ -146,6 +147,7 @@ function _scrambler_method_scramble_url(&$data) {
  *
  * @param array $parsed
  *   Parsed url array.
+ *
  * @return string
  *   Returns an unparsed url.
  */
@@ -179,6 +181,6 @@ function _scrambler_method_helper_unparse_url(array $parsed) {
  * @return string|NULL
  *   Returns the key value of the array or NULL if the key not exists.
  */
-function _scrambler_method_helper_parsed_value($parsed, $key) {
+function _scrambler_method_helper_parsed_value(array $parsed, $key) {
   return isset($parsed[$key]) ? $parsed[$key] : NULL;
 }

modules/scrambler_field/includes/scrambler_field.admin.inc
@@ -26,12 +26,12 @@ function scrambler_field_administer_fields_page() {
  * @return array
  *   Returns the generated form object.
  */
-function scrambler_field_settings_form($form, &$form_state) {
+function scrambler_field_settings_form(array $form, array &$form_state) {
 
   $menu_entries = array(
     'items' => array(
-      l('General settings', 'admin/config/development/scrambler/general'),
-      l('Field settings', 'admin/config/development/scrambler/fields'),
+      l(t('General settings'), 'admin/config/development/scrambler/general'),
+      l(t('Field settings'), 'admin/config/development/scrambler/fields'),
     ),
     'type' => 'ul',
     'title' => t('Configure the scrambling process'),
@@ -57,7 +57,7 @@ function scrambler_field_settings_form($form, &$form_state) {
  * @return array
  *   Returns the generated form object.
  */
-function scrambler_field_fields_form($form, &$form_state) {
+function scrambler_field_fields_form(array $form, array &$form_state) {
   if (
     array_key_exists('form_id', $_POST) &&
     ($_POST['form_id'] != 'scrambler_field_fields_form')
@@ -110,7 +110,7 @@ function scrambler_field_fields_form($form, &$form_state) {
  * @param array $form_state
  *   The form state of the form object to be submitted.
  */
-function scrambler_field_fields_form_submit($form, &$form_state) {
+function scrambler_field_fields_form_submit(array $form, array &$form_state) {
   $scramble = new \Drupal\scrambler_field\BLL\Scramble();
   $fields = $scramble->getScramblerFields();
   if (array_key_exists($form_state['values']['scrambler_field'], $fields)) {

modules/scrambler_field/includes/scrambler_field.entity.inc
@@ -35,7 +35,7 @@ function scrambler_field_entity_info() {
  * @return string
  *   Returns the field name.
  */
-function scrambler_field_entity_label_callback($entity, $type) {
+function scrambler_field_entity_label_callback(\Entity $entity, $type) {
   if ($type != 'scrambler_field') {
     return FALSE;
   }

modules/scrambler_field/includes/scrambler_field.view.inc
@@ -82,7 +82,7 @@ function scrambler_field_views_default_views() {
  * @param array $vars
  *   Contains all the variable information of the views_view_table theme.
  */
-function scrambler_field_preprocess_views_view_table(&$vars) {
+function scrambler_field_preprocess_views_view_table(array &$vars) {
   $scramble = new \Drupal\scrambler_field\BLL\Scramble();
   $fields = $scramble->getScramblerFields();
 

modules/scrambler_field/src/BLL/Scramble.php
@@ -49,7 +49,7 @@ class Scramble {
    * @return bool
    *   Returns TRUE if field is stored in sql.
    */
-  private function fieldStoredInSql($field) {
+  private function fieldStoredInSql(array $field) {
     return array_key_exists('storage', $field) &&
       array_key_exists('type', $field['storage']) &&
       ($field['storage']['type'] == 'field_sql_storage');
@@ -115,7 +115,7 @@ class Scramble {
    * @return NULL|string
    *   Returns the table name, on failure returns NULL.
    */
-  private function getFieldTableName($field, $type) {
+  private function getFieldTableName(array $field, $type) {
     if (isset($field['storage']) &&
         isset($field['storage']['details']) &&
         isset($field['storage']['details']['sql']) &&
@@ -127,4 +127,5 @@ class Scramble {
 
     return NULL;
   }
+
 }

modules/scrambler_field/src/Config/Variable.php
@@ -10,27 +10,47 @@ namespace Drupal\scrambler_field\Config;
  * Variable class.
  */
 class Variable {
+
+  /**
+   * Get general settings.
+   *
+   * @return array
+   *   Returns the general scrambler settings array.
+   */
   private function getGeneralSettings() {
     return variable_get('scrambler_general', array());
   }
-  
+
+  /**
+   * Get the title from the settings.
+   *
+   * @return int
+   *   Returns 0 or 1.
+   */
   public function getTitle() {
     $general = $this->getGeneralSettings();
 
     if (array_key_exists('title', $general)) {
       return $general['title'];
-    } 
+    }
 
     return 0;
   }
 
+  /**
+   * Get the content types.
+   *
+   * @return array
+   *   Returns an array of content types.
+   */
   public function getContentTypes() {
     $general = $this->getGeneralSettings();
 
     if (array_key_exists('content_types', $general)) {
       return $general['content_types'];
-    } 
+    }
 
     return array();
   }
+
 }

modules/scrambler_ui/includes/scrambler_ui.admin.inc
@@ -75,7 +75,7 @@ function scrambler_ui_administration_process_page() {
  * @param array $context
  *   Drupal's context for batch processing.
  */
-function scrambler_ui_administration_processor($module, $group, &$context) {
+function scrambler_ui_administration_processor($module, $group, array &$context) {
   $api = new scrambler\API();
   $object = $api->prepareScramblerObject($module, $group);
   $object->execute();

commit e8eced6112c81936fb7621ca1982ca65e3baf8d5

Issue by Novitsh: Fix issue introduced by 5a8d59f by correcting  as a DatabaseStatementBase.

src/API.php
@@ -85,7 +85,7 @@ class ImplementationObject {
    * @return array
    *   Returns all records where the methods have been applied.
    */
-  private function applyMethod(array $records) {
+  private function applyMethod(\DatabaseStatementBase $records) {
     $up_records = array();
     $function = $this->method;
 